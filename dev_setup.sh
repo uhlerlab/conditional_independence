1f212d23cd3b181fbdad96fbdd1cabccbe6cd8bd
Chandler <chandlersquires@gmail.com>
2021-01-31 18:24:56 -0500

    release

--- dev_setup.sh
+++ dev_setup.sh
@@ -2,5 +2,5 @@
 
 python3 -m venv venv
 source venv/bin/activate
-pip3 install ipython numpy numexpr ipdb tqdm pygam scikit-learn
+pip3 install ipython numpy numexpr ipdb tqdm pygam scikit-learn twine wheel
 pip3 install jedi==0.17.2
