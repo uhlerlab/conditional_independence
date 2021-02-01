f7056ae4f280caa5ef8d31704a7416eeae5ec155
Chandler <chandlersquires@gmail.com>
2021-01-31 20:32:09 -0500

    remove import

--- sphinx_docs/conf.py
+++ sphinx_docs/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join('..')))
-from conditional_independence import *
+# from conditional_independence import *
 
 
 # -- Project information -----------------------------------------------------
