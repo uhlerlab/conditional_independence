3a0fc84d1c26fa5d13fe2ec02807a72fe51105e1
Chandler <chandlersquires@gmail.com>
2021-01-31 20:57:23 -0500

    add requirements

--- sphinx_docs/conf.py
+++ sphinx_docs/conf.py
@@ -14,7 +14,7 @@
 
 import os
 import sys
-# sys.path.insert(0, os.path.abspath(os.path.join('..')))
+sys.path.insert(0, os.path.abspath(os.path.join('..')))
 # from conditional_independence import *
 
 
