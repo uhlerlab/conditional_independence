3d213bc38234e661d6d09340770c60547af55807
Chandler <chandlersquires@gmail.com>
2021-02-01 01:02:51 -0500

    installs for travis

--- dev_setup.sh
+++ dev_setup.sh
@@ -3,4 +3,5 @@
 python3 -m venv venv
 source venv/bin/activate
 pip3 install ipython numpy numexpr ipdb tqdm pygam scikit-learn twine wheel sphinx_rtd_theme
+pip3 install coverage
 pip3 install jedi==0.17.2
